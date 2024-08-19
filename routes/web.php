295654b4954bc4703cf15f537968486ca0c6c4ee
Mudassir Uddin Developer <110125445+Mudassir-Uddin@users.noreply.github.com>
2024-08-19 21:50:09 +0500

    Website Template Implement

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [HomeController::class,'index']);
