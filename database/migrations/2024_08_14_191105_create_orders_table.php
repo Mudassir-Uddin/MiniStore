3f10f30b377338a0dbfb22c972e823490cc54b34
Mudassir Uddin Developer <110125445+Mudassir-Uddin@users.noreply.github.com>
2024-09-05 19:43:28 +0500

    Migrations Plus Create Database

---
+++ database/migrations/2024_08_14_191105_create_orders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('ref_no');
+            $table->integer('total_amount');
+            $table->integer('amount_tendered');
+            $table->integer('order_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
---
+++ database/migrations/2024_08_14_193834_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('CategoryID');
+            $table->foreign('CategoryID')->references('id')->on('categories');
+            $table->string('name',500);
+            $table->string('img',1000);
+            $table->text('Description');
+            $table->integer('price');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
